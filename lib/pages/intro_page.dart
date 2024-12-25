6b97ada27ab99293754ab683224da6cab2cc4eb8
Youcef Messelem <youcefmesselem@Youcefs-MacBook-Pro.local>
2024-12-25 19:11:43 +0400

    Added a home page and made adjustments on the landing page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_application_powermax/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 class IntroPage extends StatelessWidget {
@@ -8,16 +9,35 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Column(
-          children: [
-            //logo
-            Image.asset("lib/images/logo.png", height: 240,),
-            //title
-        
-            //subtitle
-        
-            //start now button
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Image.asset("lib/images/logo.png", height: 240,),
+              const SizedBox(height: 48,),
+              //title
+              const Text("Just Do It!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),),
+          const SizedBox(height: 48,),
+              //subtitle
+             const Text("Brand New Sneakers and Custom Kicks Made With Premium Materials", style: TextStyle( fontSize: 16.0, color: Colors.grey,),textAlign: TextAlign.center,),
+             const SizedBox(height: 48,),
+             GestureDetector(
+              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage())),
+               child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                padding: const EdgeInsets.all(25.0),
+                child: const Center(child: Text("Shop Now!", 
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 16.0),)),
+               ),
+             )
+              //start now button
+            ],
+          ),
         ),
       ),
     );
